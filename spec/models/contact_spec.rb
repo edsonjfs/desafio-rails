74f81ba4efe8923d37466020d435772d9f05896c
edsonjfs <edsonjosefs@gmail.com>
2022-02-07 16:56:54 -0300

    refactor: nested tests in describe

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -5,17 +5,19 @@ RSpec.describe Contact, type: :model do
   let(:user_02) { User.create(name: 'User_02 Name', age: 30, biography: 'bio') }
   let(:contact) { Contact.create(name: 'Contact Name', user_id: user.id) }
 
-  it 'is valid with valid name attribute' do
-    expect(contact).to be_valid
-  end
+  describe 'Validations' do
+    it 'is valid with valid name attribute' do
+      expect(contact).to be_valid
+    end
 
-  it 'is not valid when two contacts with same name in same user' do
-    contact
-    expect(Contact.create(name: 'Contact Name', user_id: user.id)).to_not be_valid
-  end
+    it 'is not valid when two contacts with same name in same user' do
+      contact
+      expect(Contact.create(name: 'Contact Name', user_id: user.id)).to_not be_valid
+    end
 
-  it 'is valid when adding contacts with same name in diferent users' do
-    contact
-    expect(Contact.create(name: 'Contact Name', user_id: user_02.id)).to be_valid
+    it 'is valid when adding contacts with same name in diferent users' do
+      contact
+      expect(Contact.create(name: 'Contact Name', user_id: user_02.id)).to be_valid
+    end
   end
 end
